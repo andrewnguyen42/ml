d9ee8b049fb81b89924972c83d0c51aecc0a0973
Andrew Nguyen <andrew.nguyen42@gmail.com>
2016-09-29 23:33:41 -0500

    added regularized cost function

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -18,7 +18,10 @@ grad = zeros(size(theta));
 %               derivatives of the cost w.r.t. each parameter in theta
 
 
+J = 1/m.*(-y.'*log(sigmoid(X*theta))-(1-y).'*log(1-sigmoid(X*theta)))+lambda/(2*m)*([0;theta(2:end,1)].'*[0;theta(2:end,1)]);
 
+
+grad = 1/m.*X.'*(sigmoid(X*theta)-y) + lambda/m*[0;theta(2:end,1)];
 
 
 
